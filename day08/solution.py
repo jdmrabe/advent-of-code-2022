6e09758e958aea4390a1dea9e154541b107d0481
John Rabe <128967275+jdmrabe@users.noreply.github.com>
2023-04-24 07:55:21 +0800

    Update solution.py

--- day08/solution.py
+++ day08/solution.py
@@ -2,6 +2,9 @@ tree_map = []
 trees_visible = 0
     
 def on_edge(x, y)->bool:
+    """
+    Check if tree is at the edge of the tree map
+    """
     global trees_visible
     edge_value = [0, (len(tree_map[0]) - 1)]
     if x in edge_value or y in edge_value:
@@ -11,18 +14,38 @@ def on_edge(x, y)->bool:
 
 def visible_x(x, y)->bool:
     global trees_visible
-    for i in range(len(tree_map)):
-        if tree_map[x][i] >= tree_map[x][y] and i != y:
-            return False
-        else:
+    """
+    Check if any of the sides are all shorter; visible
+    """
+    left_half = tree_map[x][:y]
+    if left_half:
+        if max(left_half) < tree_map[x][y]:
             return True
+    right_half = tree_map[x][y+1:]
+    if right_half:
+        if max(right_half) < tree_map[x][y]:
+            return True
+    # Neither half has all shorter trees; not visible
+    return False
 
 def visible_y(x, y)->bool:
     global trees_visible
-    for i in range(len(tree_map[0])):
-        if tree_map[i][y] >= tree_map[x][y] and i != x:
-            return False
-    return True
+    """
+    Check if any of the sides are all shorter; visible
+    """
+    top_half = [tree_map[i][y] for i in range(x - 1, -1, -1)]
+    if top_half:
+        if max(top_half) < tree_map[x][y]:
+            return True
+    bottom_half = [tree_map[i][y] for i in range(x + 1, len(tree_map))]
+    if bottom_half:
+        if max(bottom_half) < tree_map[x][y]:
+            return True
+    #Neither half has all shorter trees; not visible
+    return False
+    
+
+
 
 if __name__ == "__main__":
 
@@ -40,4 +63,3 @@ if __name__ == "__main__":
                     trees_visible += 1
 
     print(trees_visible)
-
